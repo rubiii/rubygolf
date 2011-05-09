cb5c527b2b150aa0c886915dd23929eed88ef74d
Christopher de Bruin <christopher.debruin@mindmatters.de>
2011-05-09 20:30:54 +0200

    basic solutions to hole3 and hole6 (not optimized yet)

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,19 @@
 class Golf
+
+  def self.hole3(n)
+    r = n
+    for c in 1..(n-1) do r = r * c; end
+    r
+  end
+  
+  def self.hole6(n)
+    r = []
+    for c in 1..n do
+      t = c%3
+      f = c%5
+      r << (t == 0 && f == 0 ? "fizzbuzz" : (t == 0 ? "fizz" : (f == 0 ? "buzz" : c)))
+    end
+    r
+  end
+  
 end
