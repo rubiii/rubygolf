27099fe89c2c1ee68cce7d2ca401e04d31b03290
Christopher de Bruin <christopher.debruin@mindmatters.de>
2011-05-09 20:34:37 +0200

    small optimization

--- lib/golf.rb
+++ lib/golf.rb
@@ -9,11 +9,11 @@ class Golf
   def self.hole6(n)
     r = []
     for c in 1..n do
-      t = c%3
-      f = c%5
-      r << (t == 0 && f == 0 ? "fizzbuzz" : (t == 0 ? "fizz" : (f == 0 ? "buzz" : c)))
+      t = c%3 == 0
+      f = c%5 == 0
+      r << (t && f ? "fizzbuzz" : (t ? "fizz" : (f ? "buzz" : c)))
     end
     r
   end
-  
+
 end
